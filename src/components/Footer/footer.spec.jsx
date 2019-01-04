d0b7908ce66590519e0a93156200219e0c8adbd1
TopKeingt <ppkhiemnguyen@gmail.com>
2019-01-04 12:29:26 -0500

    add a test 'if ContactForm render properly'

--- src/components/Footer/footer.spec.jsx
+++ src/components/Footer/footer.spec.jsx
@@ -1,11 +1,22 @@
 import React from "react"
-import { shallow, configure } from "enzyme"
+import { mount, shallow, configure } from "enzyme"
 import Footer from "./footer"
+import ContactForm from "../Forms/contact"
 import Adapter from "enzyme-adapter-react-16"
 configure({ adapter: new Adapter() })
 
 describe("Footer", () => {
+  let mountedFooter
+  const mountFooter = () => {
+    if (!mountedFooter) {
+      mountedFooter = mount(<Footer />)
+    }
+  }
   it("renders without crash", () => {
     shallow(<Footer />)
+  })
+  it("renders ContactForm properly", () => {
+    const wrapper = shallow(<Footer />)
+    expect(wrapper).toContainReact(<ContactForm />)
   })
 })
