e6b226aaa950f3c06712b9192104381cdc92fe4e
TopKeingt <ppkhiemnguyen@gmail.com>
2019-01-05 16:51:08 -0500

    fix netlify's form handler bug and add success message when submitted

--- src/components/Forms/contact.js
+++ src/components/Forms/contact.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { Component, PureComponent } from "react"
 
 const encode = data => {
   return Object.keys(data)
@@ -6,12 +6,12 @@ const encode = data => {
     .join("&")
 }
 
-export default class ContactForm extends Component {
+class Form extends Component {
   state = {
     form: {},
-    isSubmited: false,
   }
 
+  // every change in input will change the state of it or create a new item with the input's name
   onInputChange = event => {
     const fields = this.state.form
     const newFields = {}
@@ -22,16 +22,21 @@ export default class ContactForm extends Component {
     })
   }
 
+  // submit this to netlify's handler and reset fields
   handleSubmit = event => {
     event.preventDefault()
+    const form = this.state.form
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state.form }),
+      body: encode({ "form-name": "contact", ...form }),
     })
       .then(() => alert("Success!"))
       .catch(error => alert(error))
-    this.setState({ isSubmited: true })
+
+    this.setState({ form: {} })
+
+    this.props.Submitted()
   }
 
   render() {
@@ -40,37 +45,66 @@ export default class ContactForm extends Component {
         <input type="hidden" name="form-name" value="contact" />
         <div className="mt-10">
           <input
+            id="name"
             type="text"
             name="name"
             placeholder="name"
             className="single-input"
             value={this.state.form.name || ""}
             onChange={this.onInputChange}
+            require="true"
           />
         </div>
         <div className="mt-10">
           <input
+            id="email"
             type="email"
             name="email"
             placeholder="email"
             className="single-input"
             value={this.state.form.email || ""}
             onChange={this.onInputChange}
+            require="true"
           />
         </div>
         <div className="mt-10">
           <textarea
+            id="message"
             name="message"
-            placeholder="Message"
+            placeholder="message"
             className="single-textarea"
             value={this.state.form.message || ""}
             onChange={this.onInputChange}
+            require="true"
           />
         </div>
         <button type="submit" className="genric-btn primary mt-10 btn-block">
           Submit
         </button>
       </form>
+    )
+  }
+}
+
+export default class ContactForm extends PureComponent {
+  state = {
+    isSubmited: false,
+  }
+
+  Submitted = () => {
+    this.setState({ isSubmited: true })
+  }
+
+  render() {
+    const { isSubmited } = this.state
+    return (
+      <React.Fragment>
+        {isSubmited ? (
+          <p className="text-center text-white">Thanks For Your Submission</p>
+        ) : (
+          <Form Submitted={this.Submitted} />
+        )}
+      </React.Fragment>
     )
   }
 }
