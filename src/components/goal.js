4d40856248065fe9b0186ce482149f0b71c7f870
TopKeingt <ppkhiemnguyen@gmail.com>
2019-01-06 13:47:45 -0500

    replace junk text with lorem

--- src/components/goal.js
+++ src/components/goal.js
@@ -1,5 +1,34 @@
 import React from "react"
 
+const Text = () => {
+  return (
+    <React.Fragment>
+      <h1 className="pb-2">Our Purpose</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
+        eos. Soluta numquam iure culpa voluptatum dolore saepe laudantium
+        aliquid accusantium architecto iusto? Obcaecati, optio adipisci ratione
+        voluptatum dolores iusto ducimus natus,
+      </p>
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
+        eos. Soluta numquam iure culpa voluptatum dolore saepe laudantium
+        aliquid accusantium architecto iusto? Obcaecati, optio adipisci ratione
+        voluptatum dolores iusto ducimus natus, Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Repudiandae, eos. Soluta numquam iure
+        culpa voluptatum dolore saepe laudantium aliquid accusantium architecto
+        iusto? Obcaecati, optio adipisci ratione voluptatum dolores iusto
+        ducimus natus,
+      </p>
+    </React.Fragment>
+  )
+}
+
+const Image = () => {
+  return <img className="img-fluid" src="/assets/img/about-img.jpg" alt="" />
+}
+
 const Goal = props => {
   return (
     <section className="info-area pb-120">
@@ -7,33 +36,10 @@ const Goal = props => {
         <div className="container-fluid">
           <div className="row align-items-center">
             <div className="col-lg-6 no-padding info-area-left">
-              <img
-                className="img-fluid"
-                src="/assets/img/about-img.jpg"
-                alt=""
-              />
+              <Image />
             </div>
             <div className="col-lg-6 info-area-right">
-              <h1>Our Purpose</h1>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Repudiandae, eos. Soluta numquam iure culpa voluptatum dolore
-                saepe laudantium aliquid accusantium architecto iusto?
-                Obcaecati, optio adipisci ratione voluptatum dolores iusto
-                ducimus natus,
-              </p>
-              <br />
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Repudiandae, eos. Soluta numquam iure culpa voluptatum dolore
-                saepe laudantium aliquid accusantium architecto iusto?
-                Obcaecati, optio adipisci ratione voluptatum dolores iusto
-                ducimus natus, Lorem, ipsum dolor sit amet consectetur
-                adipisicing elit. Repudiandae, eos. Soluta numquam iure culpa
-                voluptatum dolore saepe laudantium aliquid accusantium
-                architecto iusto? Obcaecati, optio adipisci ratione voluptatum
-                dolores iusto ducimus natus,
-              </p>
+              <Text />
             </div>
           </div>
         </div>
@@ -42,31 +48,10 @@ const Goal = props => {
           <div className="container">
             <div className="row align-items-center">
               <div className="col-lg-7">
-                <h1 className="pb-2">Our Purpose</h1>
-                <p>
-                  inappropriate behavior is often laughed off as “boys will be
-                  boys,” women face higher conduct standards especially in the
-                  workplace. That’s why it’s crucial that, as women, our
-                  behavior on the job is beyond reproach.
-                </p>
-                <br />
-                <p>
-                  inappropriate behavior is often laughed off as “boys will be
-                  boys,” women face higher conduct standards especially in the
-                  workplace. That’s why it’s crucial that, as women, our
-                  behavior on the job is beyond reproach. inappropriate behavior
-                  is often laughed off as “boys will be boys,” women face higher
-                  conduct standards especially in the workplace. That’s why it’s
-                  crucial that, as women, our behavior on the job is beyond
-                  reproach.
-                </p>
+                <Text />
               </div>
               <div className="col-lg-5 no-padding pt-5">
-                <img
-                  className="img-fluid"
-                  src="/assets/img/about-img.jpg"
-                  alt=""
-                />
+                <Image />
               </div>
             </div>
           </div>
