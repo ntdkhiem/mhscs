c01f0299ff75a5550a85bc6a4736becf3f4c1f09
TopKeingt <ppkhiemnguyen@gmail.com>
2019-01-07 18:11:18 -0500

    update gallery's loading btn

--- src/Gallery/gallery.js
+++ src/Gallery/gallery.js
@@ -1,19 +1,40 @@
 import React, { PureComponent } from "react"
+import Image from "react-image-resizer"
+
 import { getPhotos } from "../containers/google"
 import Banner from "../components/banner"
 
 class Gallery extends PureComponent {
   state = {
     photos: [],
+    pageToken: "",
+    hasMorePic: false,
   }
 
   componentDidMount() {
-    getPhotos((photos, _url = "//127.0.0.1:5000") => {
+    getPhotos(photos => {
+      photos = this.isTokenExist(photos)
       this.setState({ photos })
     })
   }
+
+  isTokenExist = photos => {
+    if (typeof photos[photos.length - 1] == "string") {
+      const pageToken = photos[photos.length - 1]
+      photos = photos.slice(0, photos.length - 1)
+      this.setState({ pageToken, hasMorePic: true })
+    } else {
+      this.setState({ hasMorePic: false })
+    }
+    return photos
+  }
+
+  morePhotos = () => {
+    this.setState({ hasMorePic: false })
+  }
+
   render() {
-    const { photos } = this.state
+    const { photos, hasMorePic } = this.state
     return (
       <React.Fragment>
         {/* <!-- start banner Area --> */}
@@ -22,37 +43,36 @@ class Gallery extends PureComponent {
 
         {/* <!-- Start gallery Area --> */}
         <section className="gallery-area section-gap">
-          <div className="container text-center">
+          <div className="container-fluid text-center">
             <div className="row justify-content-center">
               {photos.length !== 0 ? (
                 photos.map(photo => (
-                  <div key={photo.id} className="col-lg-4">
+                  <div key={photo.id} className="col-lg-3 m-3 image-card">
                     <a
                       href={photo.link}
                       target="_blank"
                       rel="noopener noreferrer"
                       className="img-gal"
                     >
-                      <div className="single-imgs relative">
-                        <div className="overlay overlay-bg" />
-                        <div className="relative">
-                          <img
-                            src={photo.thumbNail}
-                            alt=""
-                            className="img-fluid"
-                          />
-                        </div>
-                      </div>
+                      <Image src={photo.thumbNail} width={360} height={250} />
                     </a>
                   </div>
-                ))
+                ))(
+                  hasMorePic ? (
+                    <button
+                      className="genric-btn primary-border text-uppercase mt-3"
+                      onClick={this.morePhotos}
+                    >
+                      Load more
+                    </button>
+                  ) : (
+                    <p>No more image...</p>
+                  ),
+                )
               ) : (
                 <p className="text-lead">No Images Found...</p>
               )}
             </div>
-            <a href="!#" className="genric-btn primary-border text-uppercase">
-              Load more
-            </a>
           </div>
         </section>
         {/* <!-- End gallery Area -->*/}
