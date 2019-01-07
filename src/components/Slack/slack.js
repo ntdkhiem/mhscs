54aa843eefc62d638cfc3a042d32a53f8a006950
TopKeingt <ppkhiemnguyen@gmail.com>
2019-01-07 18:11:48 -0500

    replace !# to slack's invitation url

--- src/components/Slack/slack.js
+++ src/components/Slack/slack.js
@@ -11,7 +11,7 @@ const Slack = () => {
   return (
     <section className="cta-two-area">
       <div className="container text-center">
-        <a href="#">
+        <a href="https://mhscomputerclub.slack.com/">
           <img
             className="img-fluid"
             src="/assets/img/slack.png"
