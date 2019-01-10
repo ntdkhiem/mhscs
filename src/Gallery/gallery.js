76d0f458d49272029a6a05871fa9c18ab3d711f6
TopKeingt <ppkhiemnguyen@gmail.com>
2019-01-09 21:48:16 -0500

    fixup! add and move google api requests to containers/google.js

--- src/Gallery/gallery.js
+++ src/Gallery/gallery.js
@@ -1,70 +1,12 @@
 import React, { PureComponent } from "react"
 import Image from "react-image-resizer"
-import $ from "jquery"
 
 import Banner from "../components/banner"
-
-const PHOTOS_FOLDER_ID = process.env.REACT_APP_GOOGLE_DRIVE_PHOTO_FOLDER_ID
-const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
-
-var google_drive_url_params = {
-  key: API_KEY,
-  orderBy: "createdTime desc",
-  q: `"${PHOTOS_FOLDER_ID}" in parents`,
-  pageSize: "6",
-  fields: "nextPageToken, files(id)",
-}
-let google_drive_url =
-  `https://www.googleapis.com/drive/v3/files?` +
-  $.param(google_drive_url_params)
+import { getPhotos } from "../containers/google"
 
 class Gallery extends PureComponent {
-  state = {
-    photos: [],
-    pageToken: "",
-    hasMorePic: false,
-    isLoading: true,
-    error: false,
-  }
-
-  fetchPhotos(token = null) {
-    if (token) {
-      google_drive_url += "&pageToken=" + token
-    }
-    fetch(google_drive_url)
-      .then(response => response.json())
-      .then(data => {
-        const photos = this.state.photos
-        let newPhotos = []
-        data.files.map(file => {
-          return newPhotos.push({
-            id: file.id,
-            thumbNail: `//drive.google.com/thumbnail?id=${
-              file.id
-            }&sz=w360-h250`,
-            link: `//drive.google.com/open?id=${file.id}`,
-          })
-        })
-        this.setState({
-          pageToken: data.nextPageToken || "",
-          photos: [...photos, ...newPhotos],
-          hasMorePic: data.nextPageToken ? true : false,
-          isLoading: false,
-        })
-      })
-      .catch(error => this.setState({ error, isLoading: false }))
-  }
-
-  componentDidMount() {
-    this.fetchPhotos()
-  }
-
-  morePhotos = () => {
-    this.fetchPhotos(this.state.pageToken)
-  }
-
   render() {
-    const { photos, hasMorePic, isLoading, error } = this.state
+    const { photos, pageToken, isLoading, error, morePhotos } = this.props
     let galleryContent
     if (error) {
       galleryContent = <div>Error: please contact us!! ({error.message})</div>
@@ -94,10 +36,10 @@ class Gallery extends PureComponent {
               <p className="text-lead">No Images Found...</p>
             )}
           </div>
-          {hasMorePic ? (
+          {pageToken !== "" ? (
             <button
               className="genric-btn primary-border text-uppercase mt-3"
-              onClick={this.morePhotos}
+              onClick={morePhotos}
             >
               Load more
             </button>
@@ -123,4 +65,4 @@ class Gallery extends PureComponent {
   }
 }
 
-export default Gallery
+export default getPhotos(Gallery)
